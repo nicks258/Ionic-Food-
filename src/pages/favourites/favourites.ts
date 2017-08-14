54cdea76d51663b89f13e51bb4ec890ab11d915d
Sumit Mehra <mehrasumit258@gmail.com>
2017-08-14 16:18:41 +0530

    initial commit Off.

--- src/pages/favourites/favourites.ts
+++ src/pages/favourites/favourites.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { Http } from '@angular/http';
+import { NativeStorage } from '@ionic-native/native-storage';
+import 'rxjs/add/operator/map';
 /**
  * Generated class for the FavouritesPage page.
  *
@@ -13,12 +15,38 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'favourites.html',
 })
 export class FavouritesPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  favourities : any;
+  constructor(public navCtrl: NavController, public navParams: NavParams,public nativeStorage: NativeStorage,public http: Http) {
+  this.getFavourites();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad FavouritesPage');
+  }
+  getFavourites()
+  {
+    this.nativeStorage.getItem('USERID')
+      .then( (data)=>{
+
+    this.http.get('http://54.172.94.76:9000/api/v1/customers/'+data.customerId +'/favourites')
+      .map(res => res.json())
+      .subscribe(
+        data => {
+          setTimeout(() => {
+
+            this.favourities = data.data;
+            console.log(JSON.stringify(this.favourities));
+            // this.nextlength = data.data.length;
+            // loadingPopup.dismiss();
+          }, 1000);
+        },
+        err => console.error(err)
+      );
+  }, function(error){
+  console.log("Use real mobile app for getting exact data");
+  //dummy variables for browser use
+});
+
   }
 
 }
