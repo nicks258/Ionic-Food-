cd2432f58b460d2232e69b26238ee7a438da7b81
suvojitkar <suvojitkar@suvojitkar.local>
2017-09-09 19:32:11 +0530

    login data modified

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -99,6 +99,7 @@ export class LoginPage {
                 console.log("Error");
                 loading.dismiss();
                 env.alert();
+                //env.events.publish('user:created',Date.now());
                 //nav.setRoot(HomePage, {}, {animate: true, animation:'transition',duration:300, direction: 'forward'});
                 console.log(error);
                 }, 1000);
@@ -176,9 +177,9 @@ export class LoginPage {
       }, function (error) {
          loading.dismiss();
         setTimeout(() => {
-            //env.alert();
-            env.events.publish('user:created',Date.now());
-            nav.setRoot(HomePage, {}, {animate: true, animation:'transition',duration:300, direction: 'forward'});
+            env.alert();
+            //env.events.publish('user:created',Date.now());
+            //nav.setRoot(HomePage, {}, {animate: true, animation:'transition',duration:300, direction: 'forward'});
             console.log(error);
          }, 1000);
 
